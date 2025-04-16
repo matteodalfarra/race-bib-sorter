c2481e1ed2f22e63ef0347fb6dc252503f603606
Lorenzo Nesello <lorenzo.nesello@unifarco.it>
2025-04-16 15:50:31 +0200

    Aggiunta funzione preprocess_for_ocr_bis

--- classes/bib_reader.py
+++ classes/bib_reader.py
@@ -5,5 +5,5 @@ class BibReader:
         self.reader = easyocr.Reader(['it'])
 
     def read_bib(self, image_crop):
-        texts = self.reader.readtext(image_crop, detail=0, paragraph=False) # get text in the cut image
+        texts = self.reader.readtext(image_crop, detail=0, paragraph=False, allowlist='0123456789') # get text in the cut image
         return texts
--- classes/race_bib_processor.py
+++ classes/race_bib_processor.py
@@ -3,6 +3,7 @@ from .person_detector import PersonDetector
 import os
 from PIL import Image
 import cv2
+import numpy as np
 
 class RaceBibProcessor:
     def __init__(self, folder_path):
@@ -28,9 +29,39 @@ class RaceBibProcessor:
 
         # Threshold binario (puoi provare anche adaptiveThreshold)
         _, thresh = cv2.threshold(denoised, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
+        
         thresh = cv2.bitwise_not(thresh)
         return thresh
+    
+    def preprocess_for_ocr_bis(self, image):
+        # Converti in HSV per filtrare il rosso
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        # Maschera per escludere lo sfondo rosso
+        lower_red1 = np.array([0, 50, 50])
+        upper_red1 = np.array([10, 255, 255])
+        lower_red2 = np.array([160, 50, 50])
+        upper_red2 = np.array([180, 255, 255])
+
+        mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+        mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+        red_mask = cv2.bitwise_or(mask1, mask2)
+
+        # Inverti la maschera per mantenere SOLO i numeri bianchi
+        non_red_mask = cv2.bitwise_not(red_mask)
+
+        # Applica la maschera all'immagine originale
+        filtered = cv2.bitwise_and(image, image, mask=non_red_mask)
+
+        # Scala di grigi
+        gray = cv2.cvtColor(filtered, cv2.COLOR_BGR2GRAY)
+
+        # Equalizzazione + soglia
+        gray = cv2.equalizeHist(gray)
+        _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+        return thresh
+
+
     
     def process_images(self):
         # loop each file
@@ -54,7 +85,7 @@ class RaceBibProcessor:
                 x1, y1, x2, y2 = map(int, [row['xmin'], row['ymin'], row['xmax'], row['ymax']])
                 crop = image_cv2[y1:y2, x1:x2] # cut image
                 # Preprocessing prima dell'OCR
-                processed_crop = self.preprocess_for_ocr(crop)
+                processed_crop = self.preprocess_for_ocr_bis(crop)
                 
                 # Salva il crop preprocessato
                 debug_filename = f"{os.path.splitext(filename)[0]}_person{i+1}.png"
